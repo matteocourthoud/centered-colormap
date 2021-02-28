dcdb9ffa8c310c1227adab301070241f1eaf23c8
matteocourthoud <matteo.courthoud@uzh.ch>
2021-02-28 10:51:36 +0100

    asymemtry

--- example.m
+++ example.m
@@ -2,13 +2,13 @@
 
 % Example 1
 figure;
-surf(peaks(25));
+surf(peaks(25)+2);
 colormap(centered), colorbar;
 saveas(gcf,'figures/example1.png');
 
 % Example 2
 figure;
-contourf(peaks(25),25,'linestyle','none'); 
+contourf(peaks(25)-2,25,'linestyle','none'); 
 colormap(centered('BrBg')), colorbar;
 saveas(gcf,'figures/example2.png');
 
